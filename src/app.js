41c07e7257ad479c7f24b4f15ed28807e8082563
Lucky soni <luckysoni7060@gmail.com>
2025-09-23 12:18:54 +0530

    registration controller

--- src/app.js
+++ src/app.js
@@ -14,7 +14,16 @@ app.use(cors({
 app.use(express.json({limit: "10kb"}));
 app.use(express.urlencoded({extended: true,limit: "10kb"}));
 app.use(express.static("public"));
-app.use(cookieParser)
+app.use(cookieParser);
 
 
-export { app }
+// ROUTES IMPORT 
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration 
+app.use("/api/v1/users",userRouter);
+
+
+http://localhost:8000/register 
+
+;export { app }
